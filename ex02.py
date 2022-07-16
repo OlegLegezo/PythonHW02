b3eadf1e52f9b3f14aac78704f44e94ce4fe6097
OlegLegezo <oleglegezo@yandex.ru>
2022-07-16 22:46:51 +0300

    часть ряда заданного формулой

--- ex02.py
+++ ex02.py
@@ -2,15 +2,15 @@
 # Пример: пусть N = 4, тогда
 # [ 1, 2, 6, 24 ]
 
-#функции с мал букв! значения по умолчанию
+# функции с мал букв! значения по умолчанию
 def func1(a=4):
-#def func1(a: int, b: int) -> list(тайп хинтинг):
+    # def func1(a: int, b: int) -> list(тайп хинтинг):
     my_list = [1]
-    b=2
+    b = 2
     while len(my_list) < a:
         my_list.append(my_list[-1]*b)
         # .append позволяет добавлять элемент в конце списка, который уже существует
-        b+=1
+        b += 1
     print(my_list)
     return my_list
 
